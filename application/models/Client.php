92d16f51ba557a859bbdee058c51d411124df4f7
pujie <pw.prayitno@gmail.com>
2022-01-07 13:05:11 +0700

    add config call in Client

--- application/models/Client.php
+++ application/models/Client.php
@@ -2,10 +2,11 @@
 Class Client extends CI_Model{
     function __construct(){
         parent::__construct();
+        $this->load->config('padiconfig');
     }
     function gets(){
         $sql = 'select a.id,a.name,b.id bid,b.nama,a.alias,b.alias balias from clients a ';
-        $sql.= 'left outer join tmp.client202112 b on b.id=a.id ';
+        $sql.= 'left outer join '.$this->config->item('dbcompareto').'.client202112 b on b.id=a.id ';
         $sql.= 'where active="1" ';
         $ci = & get_instance();
         $que =  $ci->db->query($sql);
